5e797a48ea38bc4b6ba6db4004dfdb68d01639f3
John Tash <jtash25@gmail.com>
2022-09-27 17:30:27 -0400

    Added sample seed data files and dummy data and defined a funciton to seed the database with this data.

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,4 +2,29 @@ const sequelize = require('../config/connection');
 const { Blog, Comment, User } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const blogData = require('./blogData.json');
+const commentData = require('./commentData.json')
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+    await sequelize.sync()
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+      });
+
+      const blogs = await Blog.bulkCreate(blogData, {
+        individualHooks: true,
+        returning: true,
+      });
+
+      const comments = await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true,
+      });
+
+      process.exit(0)
+};
+
+seedDatabase();
